674cf4f040207989027e903deff36287c7019bf0
mehrpouya81 <mehrpouya81@gmail.com>
2015-12-04 00:06:49 -0800

    Create irsm.lua

---
+++ plugins/irsm.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " IrAn SpAmMeR Is KiNg Of AlLiANcEs  😘"
+end
+return {
+  description = "a plugin for iran spammer", 
+  usage = "a plugin for iran spammer",
+  patterns = {
+    "^Iran Spammer$",
+    "^Iran Spammer$",
+    "^iran spammer$",
+    "^ایران اسپمر$",
+  },
+  run = run
+}
+end
