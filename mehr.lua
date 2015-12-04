7fe71236b458051ec3a00528d3a339c55e608741
mehrpouya81 <mehrpouya81@gmail.com>
2015-12-04 00:02:33 -0800

    Create mehr.lua

---
+++ mehr.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " MeHrPoUyA Is KiNg OF bOtS 😘"
+end
+return {
+  description = "plgugin for admin", 
+  usage = "a plugin for bot admin",
+  patterns = {
+    "^/mehrpouya$",
+    "^Mehrpouya$",
+    "^mehrpouya$",
+    "^!mehrpouya$",
+  },
+  run = run
+}
+end
